e95d2ce85f0cf74ce94d483c2371cfe25e4895cc
SUHONG\topic <topic828@Naver.com>
2024-01-03 19:19:11 +0900

    상세페이지

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import TestPage from '@pages/Test'
 import HotelListPage from '@pages/HotelList'
-
+import HotelPage from '@pages/Hotel'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HotelListPage />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/hotel/:id" element={<HotelPage />} />
       </Routes>
     </BrowserRouter>
   )
