b9bf6a5a5d542fc0110c9f94d56816bc5078a642
SUHONG\topic <topic828@Naver.com>
2024-01-05 20:21:33 +0900

    카카오 공유하기

--- src/hooks/useLoadKakao.ts
+++ src/hooks/useLoadKakao.ts
@@ -9,7 +9,7 @@ declare global {
 function useLoadKakao() {
   useEffect(() => {
     const script = document.createElement('script')
-    script.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js'
+    script.src = 'https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js'
     script.async = true
 
     document.head.appendChild(script)
