41eb7d6049837beeed0e46600c7e4fc42d2e1e4f
SUHONG\topic <topic828@Naver.com>
2024-01-07 13:52:48 +0900

    로그인 네브바 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TestPage from '@pages/Test'
 import HotelListPage from '@pages/HotelList'
 import HotelPage from '@pages/Hotel'
-import useLoadKakao from '@hooks/useLoadKakao'
 import MyPage from '@pages/My'
 import SigninPage from '@pages/Signin'
+import AuthGuard from '@components/auth/AuthGuard'
+import Navbar from '@components/shared/Navbar'
 
+import useLoadKakao from '@hooks/useLoadKakao'
 function App() {
   useLoadKakao()
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HotelListPage />} />
-        <Route path="/my" element={<MyPage />} />
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/hotel/:id" element={<HotelPage />} />
-      </Routes>
+      <AuthGuard>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HotelListPage />} />
+          <Route path="/my" element={<MyPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/hotel/:id" element={<HotelPage />} />
+        </Routes>
+      </AuthGuard>
     </BrowserRouter>
   )
 }
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -6,18 +6,43 @@ import { useCallback } from 'react'
 import { colors } from '@styles/colorPalette'
 import Flex from '@shared/Flex'
 import Button from '@shared/Button'
+import useUser from '@hooks/auth/useUser'
+import Spacing from './Spacing'
 
 function Navbar() {
   const location = useLocation()
   const showSignButton =
     ['/signup', '/signin'].includes(location.pathname) === false
 
-  //TODO:
-  const user = null
+  const user = useUser()
 
   const renderButton = useCallback(() => {
     if (user != null) {
-      return <Link to="/my">{/* TODO */}</Link>
+      return (
+        <Flex align="center">
+          <Link to="/my">
+            <img
+              src={
+                user.photoURL ??
+                'https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-128.png'
+              }
+              alt="유저의 이미지"
+              width={40}
+              height={40}
+              style={{ borderRadius: '100%' }}
+            />
+          </Link>
+          <Spacing size={4} direction="horizontal" />
+          <Link to="/settings">
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/ionicons-outline-vol-2/512/settings-outline-64.png"
+              width={40}
+              height={40}
+              alt=""
+            />
+          </Link>
+        </Flex>
+      )
     }
 
     if (showSignButton) {
@@ -33,7 +58,7 @@ function Navbar() {
 
   return (
     <Flex justify="space-between" align="center" css={navbarContainerStyles}>
-      <Link to="/">홈</Link>
+      <Link to="/">Love Trip</Link>
       {renderButton()}
     </Flex>
   )
